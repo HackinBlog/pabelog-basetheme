e3bca82fbaa28480082273add6e7aec60bc303f7
Nadiar AS <codxse@gmail.com>
2019-07-30 01:00:08 +0700

    js

---
+++ assets/js/cljs/body-subscription-form.js
@@ -0,0 +1,3 @@
+Dd("body-subscription-form");var Bs={};
+function Cs(){return new U(null,4,5,Y,[Ln,new Uf(null,1,[Rn,"flex flex-column flex-row-ns bg-gold w-100 pa3 mt4 br2"],null),new U(null,2,5,Y,[ws,new Uf(null,4,[Oo,"flex flex-column w-100 w-70-ns",Do,"lh-copy fw7",lo,"outline-0 bg-dark-gray white hover-bg-1",To,"mt1 i dark-gray fw5"],null)],null),new U(null,2,5,Y,[Ln,new Uf(null,2,[Rn,"flex flex-column w-100 w-30-ns",Xn,new Uf(null,4,[co,"url(/wp-content/themes/basetheme/assets/images/emails.png)",wn,"no-repeat",Yn,"right",$n,"contain"],null)],null)],
+null)],null)}function Ds(){return xs("custom_html-4",Cs)}Fa("com.clupclup.app_script.modules.body_subscription_form.render",Ds);Cd.ac();
